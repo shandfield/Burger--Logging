0d5c85dd673fed6db796707d4b782b9703f04833
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-09-21 14:54:56 -0600

    re-named devour to devoured to ensure that the code lined up

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Just Cheese');
 INSERT INTO burgers (name) VALUES ('Naked Burger');
-INSERT INTO burgers (name, devour) VALUES ('Cheedder BBQ', true);
-INSERT INTO burgers (name, devour) VALUES ('Black and Blue', false);
-INSERT INTO burgers (name, devour) VALUES ('Truffle', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheedder BBQ', true);
+INSERT INTO burgers (name, devoured) VALUES ('Black and Blue', false);
+INSERT INTO burgers (name, devoured) VALUES ('Truffle', true);
 
