3364d06cfba496a3b1b7327144bc48b8ac783eaa
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-09-21 15:07:23 -0600

    having issues with heroku launch, editting the schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO burgers (name) VALUES ('Just Cheese');
-INSERT INTO burgers (name) VALUES ('Naked Burger');
-INSERT INTO burgers (name, devoured) VALUES ('Cheedder BBQ', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheedder BBQ', false);
 INSERT INTO burgers (name, devoured) VALUES ('Black and Blue', false);
-INSERT INTO burgers (name, devoured) VALUES ('Truffle', true);
+INSERT INTO burgers (name, devoured) VALUES ('Truffle', false);
 
